6202c961b55dc9c76ab10c753834df56c61b8873
peekknuf <peekknuf@gmail.com>
2024-01-20 17:11:20 +0100

    more basic tests

--- crawl.test.js
+++ crawl.test.js
@@ -1,9 +1,23 @@
 const { normalizeURL } = require('./crawl.js')
 const { test, expect } = require('@jest/globals')
 
-test('normalizeURL', () => {
-    const input = ''
+test('normalizeURL strip protocol', () => {
+    const input = 'https://maksym-ionutsa.com/path'
     const actual = normalizeURL(input)
-    const expected = ''
+    const expected = 'maksym-ionutsa.com/path'
+    expect(actual).toEqual(expected)
+} )
+
+test('normalizeURL strip backslash', () => {
+    const input = 'https://maksym-ionutsa.com/path/'
+    const actual = normalizeURL(input)
+    const expected = 'maksym-ionutsa.com/path'
+    expect(actual).toEqual(expected)
+} )
+
+test('normalizeURL case insensitivity', () => {
+    const input = 'https://MAKSYM-ionutsa.com/path/'
+    const actual = normalizeURL(input)
+    const expected = 'maksym-ionutsa.com/path'
     expect(actual).toEqual(expected)
 } )
