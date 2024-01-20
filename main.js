773ca9be38a92cbcdcef719234cf51f6c0913460
peekknuf <peekknuf@gmail.com>
2024-01-20 21:12:21 +0100

    we go brrrrrrr

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-console.log('hello world')
+function main() {
+    if (process.argv.length < 3) {
+        console.log('gimme a valid web ffs')
+        process.exit(1)
+    }
+
+    if (process.argv.length > 3) {
+        console.log('holds your horses buddy, one"s enough')
+        process.exit(1)
+    }
+    const theURLgiven = process.argv[2]
+    console.log(`the crawl of ${theURLgiven} goes brrrrrr`)
+}
+
+
+
+main()
